24860f212ddd76005d01d3fa62d847ebc746e0f6
Jackson Zamorano <jackson@jacksonzamorano.com>
2025-06-10 11:01:49 -0600

    feat: add uuid support

--- tusk/src/lib.rs
+++ tusk/src/lib.rs
@@ -4,8 +4,6 @@ pub mod query;
 pub mod reqres;
 pub mod server;
 pub mod urlencoded;
-/// Re-exports chrono for convience
-pub use chrono;
 pub use config::DatabaseConfig;
 pub use database::{Database, DatabaseError, DatabaseConnection};
 pub use query::{
@@ -16,8 +14,9 @@ pub use query::{
 pub use reqres::{BodyContents, Request, RequestType, Response, ResponseStatusCode, RouteError};
 pub use server::{IncomingRequest, Route, Server};
 pub use tokio_postgres::{error::SqlState, types::ToSql, Row};
-pub use tusk_rs_derive::{
-    route, treatment, PostgresReadFields, PostgresReadable, PostgresWriteFields,
-    PostgresWriteable, FromPostgres, PostgresJoins, embed, embed_binary
-};
 pub use urlencoded::{FromUrlEncoded, UrlEncoded};
+
+/// Re-exports chrono for convience
+pub use chrono;
+pub use tusk_rs_derive::*;
+pub use uuid;
